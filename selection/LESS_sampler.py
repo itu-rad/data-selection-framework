8e93b6b8a024a430509078932cd0b4030b9c1436
nikch <nikch@itu.dk>
2025-04-09 14:20:22 +0200

    LESSBasedSampler can now start/stop at different steps by config

--- selection/LESS_sampler.py
+++ selection/LESS_sampler.py
@@ -10,20 +10,43 @@ class LESSBasedSampler(SelectiveSampler):
 
     """ LESS utilizes 5% of data samples for warmup training. This can be change for experimentation"""
 
-    def __init__(self, dataset, num_replicas=None, rank=None, shuffle=True, seed=0):
+    def __init__(self, dataset, start_from_step, stop_after_step, num_replicas=None, rank=None, shuffle=True, seed=0):
       super().__init__(
           dataset, num_replicas=num_replicas, rank=rank, shuffle=shuffle, seed=seed
       )
+      self.less_step = start_from_step or 1
+      self.less_stop = stop_after_step or 4
       self.mask = None
       self.no_grad_scoring = False
     
     def pre_epoch(self) -> None:
         """Set mask to select all samples before each epoch starts"""
         
-        # STEP 1: train model on 5% of data set, and cache model locally.   
-        warmup()
+        # Use to test: python tune.py run less/recipe/test_full_finetune.py --config less/config/llama3_2/1b_full/train_less.yaml
         
-        # STEP 2:
+        # LESS Data Selection Pipeline: https://github.com/princeton-nlp/LESS
+        print("Starting LESS data selection pipeline")
+        # Step 1: Warmup training 
+        # Train model on 5% of data set, and cache model locally.
+        if self.less_step == 1 and self.less_stop >= 1:
+            print("doing step 1 of LESS")
+            warmup()
+            self.less_step += 1 
+        
+        # Step 2: Building the gradient datastore
+        if self.less_step == 2 and self.less_stop >= 2:
+            print("doing step 2 of LESS")
+            self.less_step += 1  
+        
+        # Step 3: Selecting data for a task
+        if self.less_step == 3 and self.less_stop >= 3:
+            print("doing step 3 of LESS")
+            self.less_step += 1  
+
+        # Step 4: Train with your selected data
+        if self.less_step == 4 and self.less_stop >= 4:
+            print("doing step 4 of LESS") 
+            
         sys.exit()
         
     def post_epoch(self) -> None:
