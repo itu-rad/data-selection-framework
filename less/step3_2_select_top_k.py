1e6a6a6b9a080e58823f8a032e179d0c6949116b
Daniel <dsep@itu.dk>
2025-05-02 14:17:23 +0200

    step 3.2 seemms to work, but rework is needed to ensure functionality

--- less/step3_2_select_top_k.py
+++ less/step3_2_select_top_k.py
@@ -1,46 +1,21 @@
-import argparse
+import json
 import os
 import torch
-import yaml
 import sys
-
-
 from torchtune import config
-from torchtune.datasets import ConcatDataset
 from omegaconf import DictConfig, ListConfig, OmegaConf
+from datasets import load_dataset
 
- 
 
-def parse_cfg():
-    argparser = argparse.ArgumentParser(
-        description='Script for selecting the data for training')
-    argparser.add_argument('--dataset_names', type=str,
-                           ncfg='+', help='The path to the score file')
-    argparser.add_argument('--datasets', type=str, ncfg='+',
-                           help='The path of the training file that corresponds to the score file')
-    argparser.add_argument('--target_task_name_names', type=str,
-                           ncfg='+', help='The name of the target task')
-    argparser.add_argument('--output_path', type=str,
-                           default="selected_data", help='The path to the output')
-    argparser.add_argument('--max_samples', type=int,
-                           default=None, help='The maximum number of samples')
-    argparser.add_argument('--percentage', type=float, default=None,
-                           help='The percentage of the data to be selected')
-
-    cfg = argparser.parse_cfg()
-
-    return cfg
 
 def get_dataset(cfg):
-    if isinstance(cfg.datasets, ListConfig):
-        # Test if dataset field in config with a single dataset is also considered a Listconfig
-        datasets = [config.instantiate(current_dataset) for current_dataset in cfg.datasets]
-        ds = ConcatDataset(datasets=datasets)
-    
-    else:
-        ds = config.instantiate(cfg.datasets)
-        
-    return ds
+    ds_list = []
+    for current_dataset in cfg.dataset_paths:
+        dataset_str= str(current_dataset)
+        ds= load_dataset(path=dataset_str,split=cfg.split)
+        ds_list.append(ds)
+
+    return ds_list
 
 
 def select_data_amount(cfg, total_samples):
@@ -60,7 +35,7 @@ def select_data_amount(cfg, total_samples):
     if cfg.percentage is not None:
         top_k = int(cfg.percentage * total_samples)
         cfg.max_samples = top_k
-        print(f'Percentage of selected data:{cfg.percentage}')
+        print(f'Percentage of selected data:{int(cfg.percentage*100)}%')
         print(f'top_k of data selected from percentage:{top_k}')
         return top_k
 
@@ -86,9 +61,13 @@ def setup(cfg, target_task_name):
     """
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
+    # Create parent folder structure for top-k data files for each target task. 
+    selected_data_path = os.path.join(cfg.output_path,target_task_name)
+    os.makedirs(selected_data_path, exist_ok=True)  # Creates directory if it doesn't exist, doesn't overwrite if it does
+    
+
     # Check that the lengths of dataset_names and datasets are equal
-    n_datasets = len(cfg.dataset_names)
-    assert n_datasets == len(cfg.datasets), "amount of datasets and amount of dataset names must be the same amount."
+    assert len(cfg.dataset_paths) == len(cfg.dataset_names), "amount of datasets and amount of dataset names must be the same amount."
     
     # Check that either percentage or max_samples is not None
     assert cfg.percentage is not None or cfg.max_samples is not None, "Both 'percentage' and 'max_sample' config fields cannot be 'None'"
@@ -133,43 +112,56 @@ def sort_scores(cfg, score_paths, num_samples, target_task_name):
     all_scores = torch.cat(all_scores, dim=0)
 
     # Create a tensor that keeps track of samples' index in their origin dataset
+     #file_specific_index = 0,1,2,3,4,5,6,0,1,2,3,4,5,0,1,2,3,4
     file_specific_index = torch.cat([torch.arange(line_num) for line_num in num_samples]).to(device)
     
     # Create a tensor that keeps track of samples' origin dataset
+    #data_from =0,0,0,0,0,0,0,1,1,
     data_from = torch.cat([torch.ones(line_num, dtype=torch.long)* i for i, line_num in enumerate(num_samples)]).to(device)
+    
+    
+    #all_scores = 32,43,3,1,2,5,100
+    print(f"all_scores shape:{all_scores}")
+    print(f"file_specific_index shape:{file_specific_index}")
+    print(f"data_from shape:{data_from}")
+
     
     # Sort the scores and return how they were sorted 'sorted_index'
     sorted_scores, sorted_index = torch.sort(all_scores, dim=0, descending=True)
     
+    
+    #sorted_index =  6,0,1,7,12,5
+    print(f"sorted_index shape:{sorted_index}")
+    
+    #sorted_scores = 100,32,43,5,3,2,1
+    print(f"sorted_index shape:{sorted_scores}")
+    
+    
     # Use 'sorted_index' to apply the same sorting on the other tensors
     sorted_data_from = data_from[sorted_index]
+    print(f"sorted_data_from shape:{sorted_data_from}")
+    
     sorted_file_specific_index = file_specific_index[sorted_index]
-        
+    print(f"sorted_file_specific_index shape:{sorted_file_specific_index}")
+    
+    
     # Save the sorted scores in a csv
     sorted_score_file = os.path.join(cfg.output_path, target_task_name, f"sorted.csv")  
     if not os.path.exists(sorted_score_file):
-        with open(sorted_score_file, 'w', encoding='utf-8') as file:
+        with open(sorted_score_file, 'x', encoding='utf-8',) as file:
             file.write("file name, index, score\n")
             for score, index, name in zip(sorted_scores, sorted_file_specific_index, sorted_data_from):
                 file.write(
                     f"{cfg.dataset_names[name.item()]}, {index.item()}, {round(score.item(), 6)}\n")
                 
                 
-    return sorted_scores, sorted_file_specific_index, sorted_data_from
+    return sorted_file_specific_index, sorted_data_from
 
-    #all_scores = 32,43,3,1,2,5,100
-    #sorted_scores = 100,32,43,5,3,2,1
-    #sorted_index =  6,0,1,7,12,5
-    #file_specific_index = 0,1,2,3,4,5,6,0,1,2,3,4,5,0,1,2,3,4
-    #data_from =0,0,0,0,0,0,0,1,1,
+  
 
 def select_and_write_samples(cfg, sorted_file_specific_index, sorted_data_from, num_samples, top_k, target_task_name):
 
-    #
-    all_lines = []
-    for i, train_file in enumerate(cfg.datasets):
-        with open(train_file, 'r', encoding='utf-8', errors='ignore') as file:
-            all_lines.append(file.readlines()[:num_samples[i]])              
+    datasets = get_dataset(cfg)
     
     # Extract top_k from both tensors
     final_index_list = sorted_file_specific_index[:top_k].tolist()
@@ -182,22 +174,26 @@ def select_and_write_samples(cfg, sorted_file_specific_index, sorted_data_from, 
         
     with open(os.path.join(cfg.output_path, target_task_name, f"top_{data_amount_name}.jsonl"), 'w', encoding='utf-8', errors='ignore') as file:
         for index, data_from in zip(final_index_list, final_data_from):
-            try:
-                file.write(all_lines[data_from][index])
-            except:
-                import pdb
-                pdb.set_trace()
-    
+                print(datasets[data_from][index])
+                file.write(json.dumps(datasets[data_from][index]) + "\n")
 
-def select_top_k(cfg:DictConfig="./data-selection-framework/less/config/llama3_2/step3.2_select_top_k.yaml") -> None:
+         
+
+def select_top_k(cfg:DictConfig="./less/config/llama3_2/step3_2_select_top_k.yaml") -> None:
     
     cfg = OmegaConf.load(cfg)
-    
+ 
     # Loop over each target task name
     for target_task_name in cfg.target_task_names:
-        score_paths, num_samples, top_k = setup(cfg)
+        score_paths, num_samples, top_k = setup(cfg,target_task_name)
         sorted_file_specific_index, sorted_data_from = sort_scores(cfg, score_paths, num_samples, target_task_name)
         select_and_write_samples(cfg, sorted_file_specific_index, sorted_data_from, num_samples, top_k, target_task_name)
+
+    #all_scores = 32,43,3,1,2,5,100
+    #sorted_scores = 100,32,43,5,3,2,1
+    #sorted_index =  6,0,1,7,12,5
+    #file_specific_index = 0,1,2,3,4,5,6,0,1,2,3,4,5,0,1,2,3,4
+    #data_from =0,0,0,0,0,0,0,1,1,
 
 
 if __name__ == "__main__":
